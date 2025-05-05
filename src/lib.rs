9c625320de7d975e2f3d8650d501de4f398265a4
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-05-05 17:08:30 +0100

    -

---
+++ src/lib.rs
@@ -0,0 +1,60 @@
+use std::net::TcpListener;
+use std::thread;
+use crossbeam::channel::{unbounded, Receiver, Sender};
+use once_cell::sync::Lazy;
+use tiny_http::{Header, Response, Server};
+
+pub fn serve_webpage() {
+    thread::spawn(|| {
+        let server = Server::http("0.0.0.0:8080").unwrap();
+        println!("Serving on http://0.0.0.0:8080");
+
+        for request in server.incoming_requests() {
+            let html = include_str!("index.html");
+            let response = Response::from_string(html).with_header(
+                Header::from_bytes(&b"Content-Type"[..], &b"text/html"[..]).unwrap()
+            );
+            request.respond(response).unwrap();
+        }
+    });
+}
+
+pub fn send(text: SendTypes) {
+    GLOBAL_SENDER.send(text).unwrap();
+}
+
+pub enum SendTypes {
+    Alpha,
+    Bravo,
+    Charlie(String)
+}
+
+static GLOBAL_SENDER: Lazy<Sender<SendTypes>> = Lazy::new(|| {
+    let (s, r) = unbounded();
+    thread::spawn(move || {
+        sender(r)
+    });
+    s
+});
+
+fn sender(r: Receiver<SendTypes>) {
+    let server = TcpListener::bind("127.0.0.1:3030").unwrap();
+    println!("Blocking WebSocket server running on ws://localhost:3030");
+
+    for stream in server.incoming() {
+        let stream = stream.unwrap();
+
+        // Accept the WebSocket handshake
+        let mut websocket = tungstenite::accept(stream).unwrap();
+
+        for message in r.iter() {
+            match message {
+                SendTypes::Alpha => { println!("Alpha") }
+                SendTypes::Bravo => { println!("Bravo") }
+                SendTypes::Charlie(c) => { println!("{c}") }
+            }
+
+            websocket.write_message("Example".into()).unwrap();
+        }
+    }
+}
